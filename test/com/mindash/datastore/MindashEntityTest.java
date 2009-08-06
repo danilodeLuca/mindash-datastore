cafb2688de787c08bc0c1272a23063c2ea6e8d6a
lugiebear <lugiebear@gmail.com>
2009-08-06 15:48:10 +0000

    Added test skeleton MindashEntityTest.java

---
+++ test/com/mindash/datastore/MindashEntityTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2009 Tristan Slominski
+ * 
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mindash.datastore;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test for <code>MindashEntity</code>.
+ * @author Tristan Slominski
+ *
+ */
+public class MindashEntityTest {
+
+  /** TEST CONSTRUCTORS */
+  
+  @Test
+  public void testMindashEntityString(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityStringKey(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityStringString(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityStringStringKey(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetAppId(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetKey(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetKind(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetNamespace(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetParent(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetProperties(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityGetPropertyString(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityHasPropertyString(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntityRemovePropertyString(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntitySetPropertiesFromMindashEntity(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntitySetPropertyStringObject(){
+    assertTrue("Not implemented", false);
+  }
+  
+  @Test
+  public void testMindashEntitySetUnindexedPropertyStringObject(){
+    assertTrue("Not implemented", false);
+  }
+ 
+}
