044ed1f76db20c33d5d85a3194edf75110a05225
lugiebear@gmail.com <lugiebear@gmail.com@b3aaba08-7f89-11de-9318-ad35c478d30f>
2009-09-27 16:33:08 +0000

    Put EntityCorruptException.java and MindashDatastoreModule.java in correct package. Updated jar.

---
+++ trunk/src/com/mindash/datastore/MindashDatastoreModule.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2009 Tristan Slominski
+ * 
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mindash.datastore;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.inject.AbstractModule;
+
+/**
+ * Google Guice Mindash Datastore Module for dependency injection.
+ * 
+ * @author Tristan Slominski
+ */
+public class MindashDatastoreModule extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    bind(DatastoreHelper.class).to(DatastoreHelperImpl.class);
+    bind(DatastoreService.class).toInstance(
+        DatastoreServiceFactory.getDatastoreService());
+    bind(MindashDatastoreService.class).to(MindashDatastoreServiceImpl.class);
+  }
+
+}
